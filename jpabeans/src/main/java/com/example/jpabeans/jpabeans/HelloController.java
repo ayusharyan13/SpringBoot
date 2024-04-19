d6029137bdf5c63159aaf62fa4c4cd7a342c9f1a
Ayush Aryan <92577214+ayusharyan13@users.noreply.github.com>
2024-04-19 20:49:04 +0530

    Java Peristent API

---
+++ jpabeans/src/main/java/com/example/jpabeans/jpabeans/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.jpabeans.jpabeans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    // for injecting some other bin
+
+    @Autowired
+    HelloWorld helloWorld;
+
+    @GetMapping("/func")
+    public void func() {
+        System.out.println(helloWorld);
+    }
+}
+
+
+// JPa is only for sql db ; not works for nosql db
